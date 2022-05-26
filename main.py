cf5700cdcac1b1c16ff04402f94d75e4c02a4e19
Andrew <and.u97@mail.ru>
2022-05-26 19:42:19 +0300

    comment

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from jnius import autoclass
+# Если вы будете создавать APK файл, то раскомментируйте строки ниже
+# Они закомментированы для корректного отображения на ПК
+#BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
+#BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
+#BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
+#UUID = autoclass('java.util.UUID')
 
-BluetoothAdapter = autoclass('android.bluetooth.BluetoothAdapter')
-BluetoothDevice = autoclass('android.bluetooth.BluetoothDevice')
-BluetoothSocket = autoclass('android.bluetooth.BluetoothSocket')
-UUID = autoclass('java.util.UUID')
-
-#Соединение HC-05:
+# Соединение HC-05:
 def get_socket_stream(name):
     paired_devices = BluetoothAdapter.getDefaultAdapter().getBondedDevices().toArray()
     socket = None
@@ -24,79 +25,96 @@ def get_socket_stream(name):
  
 class Application(App):
     def build(self):
-        #Для входа в систему EDR
+        # Для входа в систему EDR
         self.recv_stream, self.send_stream=None,None
-        #Графический интерфейс
+        # Графический интерфейс
         Layout=BoxLayout(orientation='vertical',spacing=20,padding=(0,20))
-        self.Haut_Layout=GridLayout(cols=1,size_hint=(0.3,0.3),pos_hint={'x':.1, 'y':.1})
-        self.orientation='vertical' #Максимальное количество столбцов
+        self.Haut_Layout=GridLayout(cols=2, size_hint=(0.6,0.1),pos_hint={'x':.05, 'y':.1})
+        self.orientation='veritcal' # Максимальное количество столбцов
         
-        #Добавляем в главное окно
+        # Добавляем в главное окно
         Layout.add_widget(self.Haut_Layout)
         
-        self.BoutonConnect=Button(text='Соединение')
+        self.BoutonConnect=Button(text='Connection')
         self.BoutonConnect.bind(on_press=self.connect)
-        #Добавляем кнопку на дисплее
+        # Добавляем кнопку на дисплее
         self.Haut_Layout.add_widget(self.BoutonConnect)
+                
+        self.BoutonConnect1=Button(text='Line')
+        self.BoutonConnect1.size_hint=(0.8, 0.2)
+        self.BoutonConnect1.pos_hint={'x':.5, 'y':.5}
+        self.BoutonConnect1.bind(on_press=self.send)
+        self.BoutonConnect1.id='6'
+        # Добавляем кнопку на дисплее
+        self.Haut_Layout.add_widget(self.BoutonConnect1)
         
-        
-        self.Bas_Layout=GridLayout(cols=3,size_hint=(0.8,0.7),pos_hint={'x':.1, 'y':.1},spacing=20)
-        self.orientation='horizontal' #Максимальное количество столбцов
-        self.spacing=8 #Пространство между объектами
-        self.padding=30 #Внутренние поля макета
-        
-        #Добавляем в главное окно
+        self.Bas_Layout=GridLayout(cols=3, spacing=10, size_hint=(0.9, 0.9),pos_hint={'x':.05, 'y':.1})
+
+        # Добавляем в главное окно
         Layout.add_widget(self.Bas_Layout)
         
-        #Создаем кнопку
-        self.Bouton1=Button(text='Вперед')
-        #Задаем размер в процентах
-        self.Bouton1.size_hint=(0.5,0.5)
-        #Позиция
-        self.Bouton1.pos_hint={0.5: 0.5}
-        #1 цвет фона
-        #self.Bouton1.background_normal="go.png"
-        self.Bouton1.bind(on_press=self.send)
-        self.Bouton1.id='1'
-        #Добавляем в основное окно
+        # Создаем кнопку
+        self.Bouton1=Button()
+        # 1 цвет фона
+        self.Bouton1.background_color=[0,0,0,0]
+        # Добавляем в основное окно
         self.Bas_Layout.add_widget(self.Bouton1)
+       
+        # Создаем кнопку
+        self.Bouton2=Button()
         
-        #Создаем кнопку
-        self.Bouton2=Button(text='Назад')
-        #Задаем размер в процентах
-        self.Bouton2.size_hint=(0.5,0.5)
-        #Позиция
-        self.Bouton2.pos_hint={0.6: 0.6}
-        #Цвет фона
-        #self.Bouton2.background_normal="back.png"
+        self.Bouton2.background_normal="images/go1.png"   
+        self.Bouton2.background_down="images/go.png"           
         self.Bouton2.bind(on_press=self.send)
-        self.Bouton2.id='2'
-        #Добавляем в основное окно
+        self.Bouton2.id='1'
+        # Добавляем в основное окно
         self.Bas_Layout.add_widget(self.Bouton2)
         
-         
-        self.Bouton3=Button(text='Стоп')
-        self.Bouton3.size_hint=(0.5,0.5)
-        self.Bouton3.pos_hint={0.9: 0.9}
-        #self.Bouton3.background_normal="stop.png"
+        self.Bouton3=Button(text="STOP", font_size=40)
+        self.Bouton3.color = (1, 0, 0, 1)
+        self.Bouton3.background_color=[0.3,0.3,0.3,0.3]
+        
         self.Bouton3.bind(on_press=self.send)
         self.Bouton3.id='3'
         self.Bas_Layout.add_widget(self.Bouton3)
         
+        self.Bouton4=Button()
+        self.Bouton4.background_down="images/left.png"  
+        self.Bouton4.background_normal="images/left1.png"  
+        self.Bouton4.bind(on_press=self.send)
+        self.Bouton4.id='4'
+        # Добавляем в основное окно
+        self.Bas_Layout.add_widget(self.Bouton4)
+
+        self.Bouton5=Button()
+        self.Bouton5.background_down="images/back.png"  
+        self.Bouton5.background_normal="images/back1.png"  
+        self.Bouton5.bind(on_press=self.send)
+        self.Bouton5.id='2'
+        # Добавляем в основное окно
+        self.Bas_Layout.add_widget(self.Bouton5)
+        
+        self.Bouton6=Button()
+        self.Bouton6.background_down="images/rigth.png"  
+        self.Bouton6.background_normal="images/rigth1.png"  
+        self.Bouton6.bind(on_press=self.send)
+        self.Bouton6.id='5'
+        # Добавляем в основное окно
+        self.Bas_Layout.add_widget(self.Bouton6)
         
         return Layout
  
     def connect(self,instance):
         try:
-            self.recv_stream, self.send_stream = get_socket_stream('HC-05')
+            self.recv_stream, self.send_stream = get_socket_stream("HC-05")
         except:
             instance.background_color=[1,0,0,1]
         if self.send_stream!=None:
             instance.background_color=[0,1,0,1]
-            instance.text="Соединение установлено"
-        #else:
-        #    instance.background_color=[1,0,0,1]
-        #    instance.text="Соединение не установлено"
+            instance.text="Connected"
+        else:
+            instance.background_color=[1,0,0,1]
+            instance.text="No connection"
  
     def send(self, instance):
         if self.send_stream!=None:
